0b49b989474273407523ecad6f73cf3fd5397a3f
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-07-27 08:27:28 -0300

    Melhoria ex 15

--- semana01/ex15.js
+++ semana01/ex15.js
@@ -2,22 +2,23 @@ let value1 = 90;
 let value2 = 7;
 let value3 = 9;
 
-if (value1 >= value2 && value1 >= value3) {
-  //value 1 biggest
+const value1IsBiggest = value1 >= value2 && value1 >= value3;
+const value2IsBiggest = value2 >= value3 && value2 >= value1;
+
+if (value1IsBiggest) {
   if (value2 >= value3) {
     console.log(value3, value2, value1);
   } else {
     console.log(value2, value3, value1);
   }
-} else if (value2 >= value3 && value2 >= value1) {
-  //value 2 biggest
+} else if (value2IsBiggest) {
   if (value3 >= value1) {
     console.log(value1, value3, value2);
   } else {
     console.log(value3, value1, value2);
   }
 } else {
-  //value 3 biggest
+  //value 3 is biggest
   if (value2 >= value1) {
     console.log(value1, value2, value3);
   } else {
